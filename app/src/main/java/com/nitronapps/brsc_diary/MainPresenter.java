2445000d7a2c76cc6cfe9d8d2e9d9b260b9c397b
snitron <nvar2@yandex.ru>
2018-10-29 21:21:17 +0300

    Added MVP classes and started doing item_lesson.xml

---
+++ app/src/main/java/com/nitronapps/brsc_diary/MainPresenter.java
@@ -0,0 +1,12 @@
+package com.nitronapps.brsc_diary;
+
+import com.arellomobile.mvp.InjectViewState;
+import com.arellomobile.mvp.MvpPresenter;
+
+
+@InjectViewState
+public class MainPresenter extends MvpPresenter<MainView> {
+        MainPresenter(){}
+
+
+}
---
+++ app/src/main/java/com/nitronapps/brsc_diary/MainView.java
@@ -0,0 +1,7 @@
+package com.nitronapps.brsc_diary;
+
+import com.arellomobile.mvp.MvpView;
+
+public interface MainView extends MvpView {
+    void startLogin();
+}
